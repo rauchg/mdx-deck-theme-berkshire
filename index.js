42aa14e04226d51f93d14f127826268d97f7155c
Guillermo Rauch <rauchg@gmail.com>
2020-05-03 15:33:16 -0700

    theme

---
+++ index.js
@@ -0,0 +1,24 @@
+export default {
+  fonts: {
+    body: "TimesNewRoman,Times New Roman,Times,Baskerville,Georgia,serif",
+    monospace: '"Roboto Mono", monospace'
+  },
+  colors: {
+    text: "#000",
+    background: "#E9E9E9",
+    primary: "blue"
+  },
+  styles: {
+    Slide: {
+      alignItems: "left",
+      padding: "0 40px"
+    },
+    h1: {
+      fontWeight: "normal"
+    },
+    pre: {
+      whiteBreak: 'break-all',
+      whiteSpace: 'pre-wrap'
+    }
+  }
+};
